a2aa6d18fdecd66be83ae2b5cafc22fc2c597974
nicolasgaldino <nicolasesmael1998@gmail.com>
2022-07-20 14:33:56 -0300

    Component footer finalizado.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,14 @@ import styles from '../styles/Footer.module.css';
 export const Footer = () => {
   return (
     <>
-      <footer>
-        
+      <footer className={styles.footerContainer}>
+        <h2>Nícolas Galdino | 2022</h2>
+        <div className={styles.socialNetworks}>
+          <a href="https://www.linkedin.com/in/n%C3%ADcolas-galdino-esmael-8370ab199/" target="_blank" rel="noreferrer"><i class="fa-brands fa-linkedin"></i></a>
+          <a href="https://github.com/nicolasgaldino" target="_blank" rel="noreferrer"><i class="fa-brands fa-github"></i></a>
+          <a href="https://twitter.com/galdino_esmael" target="_blank" rel="noreferrer"><i class="fa-brands fa-twitter"></i></a>
+          <a href="https://www.instagram.com/galdino_esmael/" target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i></a>
+        </div>
       </footer>
     </>
   )
