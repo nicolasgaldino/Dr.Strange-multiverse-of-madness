e9a294c98b9e51a4103355a30e9938488ac0e7af
nicolasgaldino <nicolasesmael1998@gmail.com>
2022-07-20 11:25:18 -0300

    Component Header criado.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,26 @@ import MarvelLogo from '../img/logo-marvel.png';
 export const Navbar = () => {
   return (
     <>
-      <header>
-        
+      <header className={styles.headerContainer}>
+        <a href="https://www.marvel.com/" target="_blank" rel="noreferrer">
+          <img src={MarvelLogo} alt="Marvel Logo" />
+        </a>
+        <nav>
+          <ul className={styles.ulContent}>
+            <li className={styles.liContent}>
+              <a href="#" target="_blank">Home</a>
+            </li>
+            <li className={styles.liContent}>
+              <a href="#" target="_blank">Personagens</a>
+            </li>
+            <li className={styles.liContent}>
+              <a href="#" target="_blank">Galeria</a>
+            </li>
+            <li className={styles.liContent}>
+              <a href="#" target="_blank">Vídeos</a>
+            </li>
+          </ul>
+        </nav>
       </header>
     </>
   )
